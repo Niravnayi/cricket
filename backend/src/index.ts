f6aa8329f880422985a99bc4f5b1de06af604dee
Nirav <niravlimbachia031@gmail.com>
2025-01-10 17:13:43 +0530

    matches and matchStats route created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import usersRoutes from './routes/usersRoutes'
 import organizersRoutes from './routes/organizersRoutes'
 import teamsRoutes from './routes/teamsRoutes'
 import tournamentsRoutes from './routes/tournamentsRoutes'
+import matchesRoutes from './routes/matchesRoutes'
+import matchStatsRoutes from './routes/matchStatsRoutes'
 
 const app = express();
 const port = 4000;
@@ -12,6 +14,8 @@ app.use('/users', usersRoutes);
 app.use('/organizers',organizersRoutes)
 app.use('/teams',teamsRoutes)
 app.use('/tournaments',tournamentsRoutes)
+app.use('/matches',matchesRoutes)
+app.use('/matchStats',matchStatsRoutes)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
