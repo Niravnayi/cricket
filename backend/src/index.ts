7c0d852bc5ae409ddfa8d12981a7ccc4e590d0a9
nirav <126887558+Niravnayi@users.noreply.github.com>
2025-01-10 12:51:50 +0530

    Requesting pull

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,18 @@
-import express from 'express';
-import usersRoutes from './routes/userRoutes'
-import organizersRoutes from './routes/organizerRoutes'
-import teamsRoutes from './routes/teamRoutes'
-import tournamentsRoutes from './routes/tournamentRoutes'
+import express from "express";
+import usersRoutes from "./routes/usersRoutes";
+import organizersRoutes from "./routes/organizersRoutes";
+import teamsRoutes from "./routes/teamsRoutes";
+import tournamentsRoutes from "./routes/tournamentsRoutes";
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
-app.use('/users', usersRoutes);
-app.use('/organizers',organizersRoutes)
-app.use('/teams',teamsRoutes)
-app.use('/tournaments',tournamentsRoutes)
+app.use("/users", usersRoutes);
+app.use("/organizers", organizersRoutes);
+app.use("/teams", teamsRoutes);
+app.use("/tournaments", tournamentsRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
